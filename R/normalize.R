a54539aaaee132bc8ae80034a6c2c9f3f57adca8
Noah Degoulange <noahdegoulange@lehmann.neurobio.nru.dk>
2026-03-30 16:38:29 +0200

    generic function example 'normalize.R'

---
+++ R/normalize.R
@@ -0,0 +1,15 @@
+normalize <- function(object){
+  UseMethod("normalize")
+}
+
+normalize.matrix <- function(object){
+  
+  print("a")
+  
+}
+
+normalize.list <- function(object){
+  
+  print("b")
+  
+}
