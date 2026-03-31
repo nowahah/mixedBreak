0ab2295064264cbb3bfff7759cce3a4bb2145fb0
Noah Degoulange <noahdegoulange@lehmann.neurobio.nru.dk>
2026-03-31 13:51:45 +0200

    Save before adding every parametrization possible to 'simData.R'

--- R/simData.R
+++ R/simData.R
@@ -4,9 +4,13 @@
 ##' @description Simulates trajectories of SDI in patients after psilocybin intake
 ##'
 ##' @param n.obs [integer >= 1] number of observations to simulate in the dataset.
-##' @param breakpoints [data.frame] data.frame with at first column containing breakpoints values
-##' (should be between 0 and the end of the trajectory)
-##' and second column with noise parameter for each breakpoint.
+##' @param score.sd [numeric>0] noise level (sd) on the measurement.
+##' TODO - later could be a vector to specify noise level in each phase of the experiment
+##' (typically lower during the plateau phase)
+##' @param times [named list or integer vector] times and noise (if any) of simulated measurements,
+##' for regular strategies.
+##' Can be a vector with values of time measurements to specify other irregular strategies.
+##'
 ##' @param b.onset [named list] named list with \itemize{
 ##' \item "value" for coefficient value,
 ##' \item "sd" for the noise level,
@@ -17,11 +21,15 @@
 ##' \item "sd" for the noise level,
 ##' \item "max" for maximum value simulated.
 ##' }
-##' @param score.sd [numeric>0] noise level (sd) on the measurement.
-##' TODO - later could be a vector to specify noise level in each phase of the experiment
-##' @param times [named list or integer vector] times and noise (if any) of simulated measurements,
-##' for regular strategies.
-##' Can be a vector with values of time measurements to specify other irregular strategies.
+##' @param plateau [named list] named list with \itemize{
+##' \item "value" for the height / altitude of the plateau (e.g. its 'y' coordinate),
+##' \item "duration" for the duration of the plateau on the same time scale as \code{times}.
+##' }
+##' @param breakpoints [data.frame] data.frame with \itemize{
+##' \item first column containing breakpoints values
+##' (should be between 0 and the end of the trajectory)
+##' \item and second column with noise parameter for each breakpoint.
+##'
 ##' @param na.prob [0<numeric<1] proportion of missing at random values in the dataset.
 ##' @param outlier.prob [0<numeric<1] proportion of outlier in the dataset.
 ##' Outliers are simulated as a random drop of 1-3 points in SDI.
@@ -29,62 +37,64 @@
 ##'
 ##'
 ##' @return A dataset with n.obs trajectories of SDI, simulated according to specified parameters
+##' It contains ID, time of measurements, Data Generation Model, simulated measurements
+##' and other characteristics such as outlier or missing value flag
 
 
-simData <- function(n.obs, breakpoints, b.onset, b.return, score.sd,
-                    times = list("value" = 20, "sd" = 0),
+simData <- function(n.obs, score.sd, times = list("value" = 20, "sd" = 0),
+                    b.onset = NULL, b.return = NULL, plateau = NULL,
+                    breakpoints = NULL, ending.times = NULL,
                     outlier.prob = 0, na.prob = 0, n.trail = 0L) {
-  
   require(dplyr)
-  
+
   ## check entries of the function
   # TODO
-  
+
   ## normalize parametrization of trajectories
   # TODO
-  
-  
-  
+
+
   ## simulate breakpoints value
   n.break <- nrow(breakpoints) ## nb of breakpoints
   bp.list <- lapply(1:n.break, function(i) {
     rnorm(n.obs, mean = breakpoints$bp[i], sd = breakpoints$bp.sd[i])
   })
   break.value <- as.data.frame(setNames(bp.list, paste0("break.", 1:n.break))) # every bp value once
-  
-  
+
+
   ## Simulate slopes coefficients
   coef.onset <- pmax(b.onset[["value"]] + rnorm(n.obs, sd = b.onset[["sd"]]), b.onset[["min"]])
   coef.return <- pmin(b.return[["value"]] + rnorm(n.obs, sd = b.return[["sd"]]), b.return[["max"]])
 
-  
+
   ## check breakpoint validity
-  # TODO - add comments
-  peaks = coef.onset/20*break.value[[1]]
-  break.value[[1]][peaks>10] = 10/(coef.onset[peaks>10]/20)
-  
-  
+  # If plateau is computed above 10 - the upper bound of the scale here -
+  # we recompute the first breakpoint so the plateau's value is exactly 10.
+  peaks <- coef.onset / 20 * break.value[[1]]
+  break.value[[1]][peaks > 10] <- 10 / (coef.onset[peaks > 10] / 20)
+
+
   ## Compute ending time of trajectories
   # !!! Only works for patterns "101" and "11"
   ending.times <- break.value[[ncol(break.value)]] - coef.onset * break.value[[1]] / coef.return
   end.time <- max(ending.times)
-  
-  
+
+
   ## Derive times of measurements
   if (length(times[["value"]]) == 1) { # regular measurements
     time <- rep(seq(0, end.time + times[["value"]], by = times[["value"]]), n.obs)
     time.step <- times[["value"]]
   } else { # irregular measurements
     time <- rep(times[["value"]], n.obs)
-    time.step <-  min(diff(times[["value"]]))
+    time.step <- min(diff(times[["value"]]))
   }
   n.time <- length(time) / n.obs
   # Adding noise to the time measurements
   if (times[["sd"]] > 0) {
-    time[time!=0] <- round(pmax(time[time!=0] + rnorm(length(time[time!=0]), sd = times[["sd"]]), 0))
+    time[time != 0] <- round(pmax(time[time != 0] + rnorm(length(time[time != 0]), sd = times[["sd"]]), 0))
   }
-  
-  
+
+
   ## Simulated dataset
   sim.dataset <- data.frame(
     ID = as.factor(rep(1:n.obs, each = n.time)),
@@ -95,8 +105,8 @@ simData <- function(n.obs, breakpoints, b.onset, b.return, score.sd,
   )
   # add and rep breakpoints values and ending times
   sim.dataset <- cbind(sim.dataset, apply(break.value, 2, rep, each = n.time))
-  
-  
+
+
   ## Simulate the "true" and "measured" trajectory (noise added)
   # !!! Only for 101 model
   sim.dataset <- sim.dataset %>%
@@ -105,36 +115,37 @@ simData <- function(n.obs, breakpoints, b.onset, b.return, score.sd,
       # compute "true perfect" trajectory (101 pattern)
       # TODO rewrite with a case_when
       truth.101 = if_else(time < break.1, b.onset * (time / 20),
-                          if_else(time < break.2, plateau, plateau + b.return * (time - break.2) / 20)
+        if_else(time < break.2, plateau, plateau + b.return * (time - break.2) / 20)
       ),
       noised.101 = pmin(10, pmax(0, truth.101 + rnorm(n(), sd = score.sd))),
       # truncated end of trajectory
-      score = if_else(time < return.0 + (1+n.trail)*time.step, pmin(10, pmax(0, round(noised.101))), NA),
+      score = if_else(time < return.0 + (1 + n.trail) * time.step, pmin(10, pmax(0, round(noised.101))), NA),
       # trailing at the end: return.0 + (1+n.trail)*time.step (to derive)
       truth.101 = pmin(10, pmax(0, truth.101))
     )
-  
-  
+
+
   ## Adding outliers - patient went to the bathroom
   # Can happen at most once per patient
   sim.dataset <- sim.dataset |>
     group_by(ID) |>
-    mutate(outliers = (row_number()==sample.int(n(), 1))*rbinom(n(), 1, outlier.prob)==1, # outliers location and draft
-           # outliers is a drop in truth of 1-3 points (still bounded 0<=score<=10)
-           score = pmin(10, pmax(0, score - sample(1:3, 1, prob = c(1,4,4))*outliers)),
-           outliers = if_else(is.na(score), NA, outliers)
+    mutate(
+      outliers = (row_number() == sample.int(n(), 1)) * rbinom(n(), 1, outlier.prob) == 1, # outliers location and draft
+      # outliers is a drop in truth of 1-3 points (still bounded 0<=score<=10)
+      score = pmin(10, pmax(0, score - sample(1:3, 1, prob = c(1, 4, 4)) * outliers)),
+      outliers = if_else(is.na(score), NA, outliers)
     )
-  
-  
+
+
   ## Adding missing values (Missing Completely At Random)
   sim.dataset <- sim.dataset %>%
-    mutate(na.value = if_else(is.na(score), NA, rbinom(n(), 1, na.prob)==1),
-           score = if_else(na.value, NA, score)
+    mutate(
+      na.value = if_else(is.na(score), NA, rbinom(n(), 1, na.prob) == 1),
+      score = if_else(na.value, NA, score)
     )
-  
-  
+
+
   ## export
-  class(sim.dataset) <- append("trajData",class(sim.dataset))
+  class(sim.dataset) <- append("trajData", class(sim.dataset))
   return(sim.dataset)
 }
-
--- test/simData-test.R
+++ test/simData-test.R
@@ -11,28 +11,33 @@ breakpoints <- data.frame(
   bp = c(90, 180),
   bp.sd = c(10, 10)
 )
-b.onset <- list("value" = 2*1.4, "sd" = 2*0.5, "min" = 0.5) # onset coefficient per 20 min
-b.return <- list("value" = 2*-0.7, "sd" = 2*0.3, "max" = -0.2) # return to normal coefficient per 20 min
+b.onset <- list("value" = 2 * 1.4, "sd" = 2 * 0.5, "min" = 0.5) # onset coefficient per 20 min
+b.return <- list("value" = 2 * -0.7, "sd" = 2 * 0.3, "max" = -0.2) # return to normal coefficient per 20 min
 score.sd <- 1 # noise level on the measurements
 # different time specifications
 time.reg <- list("value" = 20, "sd" = 0) # default regular "perfect" measurement strategy
 time.noise <- list("value" = 20, "sd" = 2) # regular "perfect" measurement strategy with noise
-time.spec <- list("value" = c(0,10,20,30,40,60,120), "sd" = 1)   # specific measurement strategy
+time.spec <- list("value" = c(0, 10, 20, 30, 40, 60, 120), "sd" = 1) # specific measurement strategy
 
-outlier.prob <- 1/5
-na.prob <- 0.10
-n.trail <- 0L
+outlier.prob <- 1 / 2 # individual probability of having exactly one outlier observation
+na.prob <- 1 / 10 # measurement probability to be missing
+n.trail <- 3L # nb of trailing observations
 
 
-sim.dataset <- simData(n.obs, breakpoints, b.onset, b.return, score.sd,
-                       times = time.noise,
-                       outlier.prob = outlier.prob, na.prob = na.prob,
-                       n.trail = n.trail)
-plot(sim.dataset)  # default trajectory plot
+sim.dataset <- simData(
+  n.obs = n.obs, score.sd = score.sd, times = time.noise,
+  b.onset = b.onset,
+  b.return = b.return,
+  plateau = NULL,
+  breakpoints = breakpoints,
+  ending.times = NULL,
+  outlier.prob = outlier.prob, na.prob = na.prob,
+  n.trail = n.trail
+)
+plot(sim.dataset) # default trajectory plot
 print(sim.dataset) # data generation model
 
 # print(sim.dataset, default = T) # default method for data.frame
 print(sim.dataset, n.line = 10L, dgm = F) # dataset of observations
 (sim.measurements <- subset(sim.dataset))
-
 # Higher variance during plateau (especially if peak <≈ 10) -> not realistic (need lower variance during plateau)
